30a121fe720613d3af5c613a21ba4508e2711973
zecamilplano <ezequiasgama2@gmail.com>
2024-12-13 13:41:23 -0300

    chore: aplica cores nas celulas

---
+++ scripts/apply-colors-grid.js
@@ -0,0 +1,122 @@
+import colorsExternal from "./colors.js"
+import { elementosNaoMetal, elementosMetaisTransicao, elementosHalogenios, elementosMetaisAlcalinoTerrosos, elementosLantanideos, elementosGasesNobres, elementosOutrosMetais, elementoSemiMetais, elementosMetaisAlcalinos, elementosActinidios } from "./elements.js"
+
+export const highlightNonMetals = () => {
+  elementosNaoMetal.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.naoMetais
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightAlkaliMetals = () => {
+  elementosMetaisAlcalinos.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.metaisAlcalinos
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightSemiMetals = () => {
+  elementoSemiMetais.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.semiMetais
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightOtherMetals = () => {
+  elementosOutrosMetais.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.outrosMetais
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightLanthanides = () => {
+  elementosLantanideos.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.lantanideos
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightNoblesGas = () => {
+  elementosGasesNobres.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.gasesNobres
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightAlkalineEarthMetals = () => {
+  elementosMetaisAlcalinoTerrosos.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.metaisAlcalinoTerrosos
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightHalogens = () => {
+  elementosHalogenios.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.halogenios
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightTransitionMetal = () => {
+  elementosMetaisTransicao.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.metaisTransicao
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
+
+export const highlightActinidia = () => {
+  elementosActinidios.map(elemento => {
+
+    const elementNode = document.querySelector(`.num${elemento}`)
+    if (elementNode) {
+      elementNode.style.backgroundColor = colorsExternal.actinidios
+    } else {
+      console.warn(`Elemento com ID num${elemento} não encontrado.`);
+    }
+  })
+}
