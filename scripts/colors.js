ff34322bb52438022df66d98d7d15e401fda4e39
zecamilplano <ezequiasgama2@gmail.com>
2024-12-13 13:43:05 -0300

    chore: define as paletas de cores

---
+++ scripts/colors.js
@@ -0,0 +1,14 @@
+const colorsExternal = {
+  naoMetais: "#96cd30",
+  metaisAlcalinos: "#D89F2B",
+  semiMetais: "#44A69C",
+  outrosMetais: "#91BDCB",
+  lantanideos: "#7BDEE5",
+  gasesNobres: "#2793E0",
+  metaisAlcalinoTerrosos: "#D1C330",
+  halogenios: "#5AC3E8",
+  metaisTransicao: "#E77878",
+  actinidios: "#D49BCD"
+}
+
+export default colorsExternal
