b5754fd566a5c5edef68825119f8bd2b47b7f479
zecamilplano <ezequiasgama2@gmail.com>
2025-01-09 21:41:02 -0300

    feat(theme): adicionar arquivo js com nova paleta de cores

--- scripts/colors.js
+++ scripts/colors.js
@@ -11,4 +11,21 @@ const colorsExternal = {
   actinidios: "#D49BCD"
 }
 
-export default colorsExternal
+const coresPorGrupo = {
+  "nao metais": { claro: "#89e531", escuro: "#32580a" },
+  "gases nobres": { claro: "#3c9fe4", escuro: "#133862" }, // Azul claro e escuro
+  "metais alcalinos": { claro: "#f1b432", escuro: "#3f3100" }, // Amarelo claro e ouro
+  "metais alcalino-terrosos": { claro: "#e2e900", escuro: "#FFA500" }, // Laranja claro e escuro
+  "semimetais": { claro: "#4cb8ae", escuro: "#0d3b36" },
+  "halogênios": { claro: "#91e4ea", escuro: "#00a0a8" },
+  "outros metais": { claro: "#9ec7d4", escuro: "#377385" },
+  "metais de transição": { claro: "#f4848b", escuro: "#b20011" },
+  "lantanídeos": { claro: "#81e7eb", escuro: "#00a0a8" },
+  "actinídeos": { claro: "#e5a0d7", escuro: "#a2008d" },
+};
+
+
+export {
+  colorsExternal,
+  coresPorGrupo
+}
